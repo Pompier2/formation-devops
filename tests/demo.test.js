3124929aaf01c21609e41e1718b3d2df7a8680a8
Alice Bob <alice@eni.fr>
2018-09-25 10:44:56 +0200

    add nickname

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -6,7 +6,7 @@ var User = function(name,nickname){
 describe("Checking if the user is created correctly", function(){
 	it("should create the user with the correct name", function(){
 		var tom = new User("tom");
-	tom.name.should.be.equal("tom");
+	tom.name.should.be.equal("tom", "tommy");
 	tom.nickname.should.be.equal("tommy");
 	});
 });
