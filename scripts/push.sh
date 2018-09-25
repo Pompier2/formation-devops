353717daeeec34a74a1d387bac5195c6114c3044
Alice Bob <alice@eni.fr>
2018-09-25 11:51:12 +0200

    Num 1

---
+++ scripts/push.sh
@@ -0,0 +1,3 @@
+t remote add cleverapps https://$CLEVER_TOKEN:$CLEVER_SECRET@push-par-
+clevercloud-customers.services.clever-cloud.com/APPLICATION_ID.git 1
+git push --force --verbose cleverapps master
